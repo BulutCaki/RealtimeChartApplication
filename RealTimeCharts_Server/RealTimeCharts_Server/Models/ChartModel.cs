c6895af9877238d776679224d7c2d3c4be138d26
BulutCaki <bulutcaki023@gmail.com>
2021-06-08 14:20:58 +0300

    Project completed

---
+++ RealTimeCharts_Server/RealTimeCharts_Server/Models/ChartModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RealTimeCharts_Server.Models
+{
+    public class ChartModel
+    {
+        public List<int> Data { get; set; }
+        public string Label { get; set; }
+
+        public ChartModel()
+        {
+            Data = new List<int>();
+        }
+    }
+}
